98ddae4fc73e48f13ac0687830676ee71430ae48
scrviy <91909966+scrviy@users.noreply.github.com>
2023-04-03 15:41:30 -0300

    numbers

--- aritmeticos.js
+++ aritmeticos.js
@@ -10,6 +10,16 @@
  * ** Potenciação
  */
 
+// Uma operador com multiplicação e divisão será executada primeiro, caso não estiver parênteses na operação.
+
+/**
+ * Precedência dos operadores
+ * ()
+ * **
+ * * / &
+ * + - 
+ */
+
 const num1 = 10;
 const num2 = 3;
 console.log(num1 + num2);
@@ -18,3 +28,22 @@ console.log(num1 * num2);
 console.log(num1 / num2);
 console.log(num1 ** num2);
 console.log(num1 % num2);
+
+let contador = 1;
+console.log(contador);
+
+contador++;
+console.log(contador);
+
+contador--;
+console.log(contador);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("---------------------");
+
+for (let j = 10; j >= 0; j--) {
+    console.log(j);
+}
